321cf1f7ce3c606c37321be904b41c239d1f4ace
Tom <tahina.rakoto76@gmail.com>
2020-01-14 10:37:03 +0100

    add settings singleton

--- Application/Core/GameLogic.py
+++ Application/Core/GameLogic.py
@@ -1,20 +1,35 @@
+from Application.Core.Settings import *
 import pygame as pg
+
+
 class Game:
     def __init__(self):
         pg.init()
         pg.mixer.init()
-        self.screen = pg.display.set_mode((1200, 600))
+        self.Settings = Settings(
+                        "WORMS Motherfuckers !",
+                        1200,
+                        600,
+                        "Graphics/icon.png",
+                        "WORMS Like - Groupe 2",
+                        60,
+                        "Graphics/Fonts/Godzilla.ttf",
+                        "Graphics/Fonts/Lemon-Juice.ttf"
+                 )
+        self.screen = pg.display.set_mode((self.Settings.instance.SCREEN_WIDTH, self.Settings.instance.SCREEN_WIDTH))
+        pg.display.set_icon(self.Settings.instance.ICON)
         self.clock = pg.time.Clock()
         self.running = True
         self.playing = False
 
-    def new(self): pass
+    def new(self):
+        pass
 
     def run(self):
         # main loop
         self.playing = True
         while self.playing:
-            self.clock.tick(60)
+            self.clock.tick(Settings.instance.FPS)
             self.events()
             self.fixed_update()
             self.update()
@@ -39,12 +54,29 @@ class Game:
         # display loop
         pg.display.flip()
 
-    def show_splashscreen(self): pass
+    def show_splashscreen(self):
+        pass
 
-    def menu(self): pass
+    def menu(self):
+        self.screen.fill((5,5,30))
+        self.draw_text(self.Settings.instance.TITLE_GAME,
+                       40,
+                       (255,255,255),
+                       self.Settings.instance.SCREEN_WIDTH / 2,
+                       self.Settings.instance.SCREEN_WIDTH / 4,
+                       self.Settings.instance.FONT_TITLE)
 
-    def draw_text(self, text, size, color, x, y):
-        font = pg.font.Font(self.font_name, size)
+        self.draw_text("Presse enter",
+                       18,
+                       (255, 255, 255),
+                       self.Settings.instance.SCREEN_WIDTH / 2,
+                       self.Settings.instance.SCREEN_WIDTH / 2,
+                       self.Settings.instance.FONT_TEXT)
+
+        pg.display.flip()
+
+    def draw_text(self, text, size, color, x, y, font_name):
+        font = pg.font.Font(font_name, size)
         text_surface = font.render(text, True, color)
         text_rect = text_surface.get_rect()
         text_rect.midtop = (x, y)
